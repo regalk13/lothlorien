c83285d88f63be13bae08d33637e1f32ba3a2334
Regalk <72028266+regalk13@users.noreply.github.com>
2025-09-02 14:54:46 -0500

    fix: hyprland cursor

--- users/regalk/graphical/hyprland/extra.nix
+++ users/regalk/graphical/hyprland/extra.nix
@@ -10,6 +10,10 @@ _:
       # See https://wiki.hyprland.org/Configuring/Monitors/
 
       monitor=,1920x1080@75,auto,auto
+
+      env = HYPRCURSOR_THEME,macOS
+      env = HYPRCURSOR_SIZE,40
+      exec-once = hyprctl setcursor macOS 28
     '';
   };
 }
