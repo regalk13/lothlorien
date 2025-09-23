24ac28e487fef7783b51393bc240e84e61b4e012
Regalk <72028266+regalk13@users.noreply.github.com>
2025-09-23 17:44:37 -0500

    feat: steam and gamescope

---
+++ modules/desktop/games/steam.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+    programs = {
+    gamescope = {
+        enable = true;
+        capSysNice = true;
+    };
+    steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+    };
+    };
+    services.getty.autologinUser = "regalk";
+      environment.systemPackages = [
+   pkgs.mangohud
+  ];
+}
