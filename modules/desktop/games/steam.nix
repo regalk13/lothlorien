af312702c572f3964c508b78a2e9206f82f40907
Regalk <72028266+regalk13@users.noreply.github.com>
2025-09-23 17:48:53 -0500

    chore: remove gamescope

--- modules/desktop/games/steam.nix
+++ modules/desktop/games/steam.nix
@@ -1,16 +1,10 @@
 { pkgs, ... }:
 {
   programs = {
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
     steam = {
       enable = true;
-      gamescopeSession.enable = true;
     };
   };
-  services.getty.autologinUser = "regalk";
   environment.systemPackages = [
     pkgs.mangohud
   ];
