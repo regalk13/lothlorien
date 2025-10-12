ded0fc6cdbb26dfdbecfac79f4df4af99f3dae43
Regalk <72028266+regalk13@users.noreply.github.com>
2025-10-11 20:09:48 -0500

    fix: xdg fix for dwl

--- modules/graphical/dwl/default.nix
+++ modules/graphical/dwl/default.nix
@@ -5,18 +5,29 @@
     environment.systemPackages =
     with pkgs;
     [
+        wmenu
         foot
         dwl
         grim
         slurp
         wl-clipboard
     ];
-
-    xdg.portal.enable = true;    
-    xdg.portal.extraPortals = [
-        pkgs.xdg-desktop-portal-wlr
-    ];
-
+    xdg.portal = {
+        enable = true;
+        wlr.enable = true;
+        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+        config = {
+        common = {
+            default = [ "wlr" ];
+        };
+        pinnacle = {
+            default = [ "wlr" ];
+            "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+            "org.freedesktop.impl.portal.Screenshot" = "wlr";
+            "org.freedesktop.impl.portal.Inhibit" = "none";
+        };
+        };
+    };
     hardware.graphics = {
         package = pkgs.mesa;
         enable32Bit = true;
