cb8a576089ec51e499c4a2655c0d8ba0cbdeafba
Regalk <72028266+regalk13@users.noreply.github.com>
2025-10-12 08:51:31 -0500

    style: fmt

--- modules/graphical/dwl/default.nix
+++ modules/graphical/dwl/default.nix
@@ -1,35 +1,33 @@
 { pkgs, ... }:
 {
-    imports = [./dwl.nix];
+  imports = [ ./dwl.nix ];
 
-    environment.systemPackages =
-    with pkgs;
-    [
-        wmenu
-        foot
-        dwl
-        grim
-        slurp
-        wl-clipboard
-    ];
-    xdg.portal = {
-        enable = true;
-        wlr.enable = true;
-        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-        config = {
-        common = {
-            default = [ "wlr" ];
-        };
-        pinnacle = {
-            default = [ "wlr" ];
-            "org.freedesktop.impl.portal.ScreenCast" = "wlr";
-            "org.freedesktop.impl.portal.Screenshot" = "wlr";
-            "org.freedesktop.impl.portal.Inhibit" = "none";
-        };
-        };
+  environment.systemPackages = with pkgs; [
+    wmenu
+    foot
+    dwl
+    grim
+    slurp
+    wl-clipboard
+  ];
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config = {
+      common = {
+        default = [ "wlr" ];
+      };
+      pinnacle = {
+        default = [ "wlr" ];
+        "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+        "org.freedesktop.impl.portal.Screenshot" = "wlr";
+        "org.freedesktop.impl.portal.Inhibit" = "none";
+      };
     };
-    hardware.graphics = {
-        package = pkgs.mesa;
-        enable32Bit = true;
-    };
+  };
+  hardware.graphics = {
+    package = pkgs.mesa;
+    enable32Bit = true;
+  };
 }
--- modules/graphical/dwl/dwl.nix
+++ modules/graphical/dwl/dwl.nix
@@ -1,12 +1,15 @@
-{ config, pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
   nixpkgs.overlays = [
-    (self: super: {
+    (_self: super: {
       dwl = super.dwl.overrideAttrs (oldAttrs: rec {
         enableXWayland = true;
-        buildInputs = oldAttrs.buildInputs ++ [ pkgs.fcft pkgs.libdrm ];
+        buildInputs = oldAttrs.buildInputs ++ [
+          pkgs.fcft
+          pkgs.libdrm
+        ];
         patches = [
-            ./patches/bar.patch
+          ./patches/bar.patch
         ];
         configH = ./dwl-config.h;
       });
