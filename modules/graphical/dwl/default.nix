4a8321b778a02d192c759baf2fce3c0e5984e1ce
Regalk <72028266+regalk13@users.noreply.github.com>
2025-10-12 23:06:27 -0500

    style: fmt

--- modules/graphical/dwl/default.nix
+++ modules/graphical/dwl/default.nix
@@ -14,21 +14,21 @@
     enable = true;
     wlr.enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
- };
+  };
   hardware.opengl.enable = true;
   hardware.graphics = {
     package = pkgs.mesa;
     enable32Bit = true;
-    
+
     extraPackages = with pkgs; [
       vulkan-loader
       libva-utils
       libvdpau-va-gl
-      
+
       glfw
       glew
     ];
-    
+
     extraPackages32 = with pkgs.driversi686Linux; [
       libvdpau-va-gl
     ];
