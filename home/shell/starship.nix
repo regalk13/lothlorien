f8545d1de2131db938ccd4d1e3ca5fe7c2209245
Regalk <72028266+regalk13@users.noreply.github.com>
2025-10-14 17:27:29 -0500

    chore: remove starship

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -1,9 +1,9 @@
 { config, ... }:
 {
-  home.sessionVariables.STARSHIP_CACHE = "${config.xdg.cacheHome}/starship";
+  # home.sessionVariables.STARSHIP_CACHE = "${config.xdg.cacheHome}/starship";
 
   programs.starship = {
-    enable = true;
+    enable = false;
     enableZshIntegration = true;
     settings = {
       character = {
