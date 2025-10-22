ee5cdc83db3d171f462bac4189a35bc99ef6c9ff
Regalk <72028266+regalk13@users.noreply.github.com>
2025-10-21 20:47:56 -0500

    chore: change to default full scheme latex <temporal>

--- home/programs/latex.nix
+++ home/programs/latex.nix
@@ -2,23 +2,7 @@
 let
   tex = pkgs.texlive.combine {
     inherit (pkgs.texlive)
-      dvisvgm
-      dvipng
-      wrapfig
-      amsmath
-      ulem
-      hyperref
-      capt-of
-      apa7
-      collection-fontsrecommended
-      scalerel
-      scheme-medium
-      pgf
-      threeparttable
-      endfloat
-      biblatex
-      biblatex-apa
-      pgfplots
+      scheme-full
       ;
   };
 in
