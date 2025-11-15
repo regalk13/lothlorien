c13e3f62d5b2e8b95d8e6f2cd1f2787c925a6114
Regalk <72028266+regalk13@users.noreply.github.com>
2025-11-14 21:54:10 -0500

    chore: update wireguard

--- modules/core/wireguard.nix
+++ modules/core/wireguard.nix
@@ -4,10 +4,11 @@ _: {
       address = [ "10.8.71.3/32" ];
       dns = [ "1.1.1.1" ];
       listenPort = 51822;
+      mtu = 1420;
       peers = [
         {
           publicKey = "0RKsm29oTNoiXqu6r6z1Crnx729jBtJSDNX1yya7aT0=";
-          allowedIPs = [ "10.8.71.1/24" ];
+          allowedIPs = [ "10.8.71.1/32" "10.8.71.3/32" ];
           endpoint = "127.0.0.1:51821";
           persistentKeepalive = 25;
         }
